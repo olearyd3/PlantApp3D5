e4ccab021f7293fd64fc3fdfdd98ac4459b5c5e1
olearyd3 <olearyd3@tcd.ie>
2022-04-09 22:13:33 +0100

    fixed up profile screen and added picture and background etc

--- screens/profile.js
+++ screens/profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert, Button, Image, Platform} from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Alert, Button, Image, Platform, ImageBackground} from 'react-native';
 import { globalStyles } from '../styles/global';
 import { Fontisto, Ionicons, AntDesign, MaterialIcons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
@@ -29,6 +29,7 @@ const Profile = ({ navigation }) => {
             if(status !== 'granted') {
                 alert('Permission was not granted!')
             }
+            imageUploaded: true
         }, [])
 
     const PickImage = async() => {
@@ -42,53 +43,60 @@ const Profile = ({ navigation }) => {
         if(!result.cancelled) {
             setImage(result.uri)
         }
+        let counter = 0;
+        counter++;
     }
         return (
-            <View style={globalStyles.container}>
-                <View style={{flexDirection: "row", paddingBottom: 40}}>
-                    <TouchableOpacity onPress={pressHandler}>
-                            <View style={styles.iconView}>
-                                <Ionicons name="home-outline" style={styles.icon} size={20}/>
-                                <Text style={styles.iconText}>Home</Text>
-                            </View>
-                    </TouchableOpacity>
-                    <Text style={styles.text_header}>Profile</Text>
-                    <TouchableOpacity onPress={()=>navigation.navigate('Login')}>
-                            <View style={styles.iconView}>
-                                <AntDesign name="poweroff" style={styles.icon} size={20}/>
-                                <Text style={styles.iconText}>Log Out</Text>
-                            </View>
-                    </TouchableOpacity>
-                </View>
-                <View style={[styles.picUpload, {alignItems: 'center', justifyContent: 'center'}]}>
-                    <TouchableOpacity title={"Upload a Profile Picture"} onPress={PickImage}>
-                    <View>
-                        {image && <Image source={{uri:image}} style={{
-                            width:300,
-                            height:300,
-                            borderRadius: 300/2
-                        }}/>}
-                        <View style={{justifyContent: 'center', textAlign: 'center'}}>
-                            <MaterialIcons name="add-circle-outline" style={styles.icon} size={20}/>
-                            <Text style={styles.iconText}>Upload a Profile Picture!</Text>
-                        </View>
+            <View style={styles.container}>
+                <ImageBackground source={require('../assets/plantbackground.jpg')} resizeMode="cover" style={styles.image}>
+                    <View style={{flexDirection: "row", paddingBottom: 40}}>
+                        <TouchableOpacity onPress={pressHandler}>
+                                <View style={styles.iconView}>
+                                    <Ionicons name="home-outline" style={styles.icon} size={20}/>
+                                    <Text style={styles.iconText}>Home</Text>
+                                </View>
+                        </TouchableOpacity>
+                        <Text style={styles.text_header}>Profile</Text>
+                        <TouchableOpacity onPress={()=>navigation.navigate('Login')}>
+                                <View style={styles.iconView}>
+                                    <AntDesign name="poweroff" style={styles.icon} size={20}/>
+                                    <Text style={styles.iconText}>Log Out</Text>
+                                </View>
+                        </TouchableOpacity>
                     </View>
-                    </TouchableOpacity>
-                </View>
-                <View style={{flex:1}}>
-                    <Text style={styles.fillerText}></Text>
-                </View>
-                <View style={styles.streakStyle}>
-                    <TouchableOpacity title={"Streak Alert"} onPress={createStreakAlert}>
-                        <View style={styles.iconView}>
-                            <View style={{flexDirection: "row"}}>
-                                <Fontisto name="fire" style={styles.streakIcon} size={20}/>
-                                <Text style={styles.iconText}>  1</Text>
-                            </View>
-                            <Text style={styles.iconText}>Current Streak</Text>
+                    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                        <TouchableOpacity title={"Upload a Profile Picture"} onPress={PickImage}>
+                        <View style={styles.picUpload}>
+                            {image && <Image source={{uri:image}} style={{
+                                width:290,
+                                height:290,
+                                borderRadius: 290/2
+                            }}/>}
                         </View>
-                    </TouchableOpacity>
-                </View>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{paddingTop: 1}}>
+                        <TouchableOpacity title={"Upload a Profile Picture"} onPress={PickImage}>
+                            <View style={styles.iconView}>
+                                <Text style={styles.iconText}>Upload a new profile picture</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{flex:1}}>
+                        <Text style={styles.fillerText}></Text>
+                    </View>
+                    <View style={styles.streakStyle}>
+                        <TouchableOpacity title={"Streak Alert"} onPress={createStreakAlert}>
+                            <View style={styles.iconView}>
+                                <View style={{flexDirection: "row"}}>
+                                    <Fontisto name="fire" style={styles.streakIcon} size={20}/>
+                                    <Text style={styles.iconText}>  1</Text>
+                                </View>
+                                <Text style={styles.iconText}>Current Streak</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </ImageBackground>
             </View>
       );
     }
@@ -97,11 +105,14 @@ const Profile = ({ navigation }) => {
      const styles = StyleSheet.create({
       container: {
         flex: 1, 
-        backgroundColor: '#ffffff',
-        padding: 20,
       },
       fillerText: {
         color: '#333'
+      },
+      image: {
+        flex: 1,
+        justifyContent: "center",
+        padding: 20,
       },
       logo: {
           alignItems: 'center'
